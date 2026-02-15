ef450703a6b183618297b0f04dff80c26b5b32db
johannesosmomcdonald-dotcom <johannesosmomcdonald@gmail.com>
2026-02-15 15:13:10 +0000

    Delete server.py

--- server.py
+++
@@ -1,8 +0,0 @@
-from http.server import ThreadingHTTPServer, SimpleHTTPRequestHandler
-
-SimpleHTTPRequestHandler.extensions_map.update({
-    ".js": "text/javascript",
-    ".mjs": "text/javascript",
-})
-
-ThreadingHTTPServer(("localhost", 8000), SimpleHTTPRequestHandler).serve_forever()
